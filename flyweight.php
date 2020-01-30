d054799c8b44c7d97d2f99b0ebd0645f231abecb
Olga Nevinchana <nevinchana@rozetka.com.ua>
2020-01-30 15:35:09 +0200

    1

--- flyweight.php
+++ flyweight.php
@@ -1,45 +1,82 @@
 <?php
 
+class ShareUnitInterface{
+    public $sound;
+    public $texture;
+
+
+}
+
 abstract class UnitInterface{
+    public $sharedInfo;
     public $weight;
     public $weapon;
     public $life;
     public $coords;
     public $name;
 
+    public function __construct(){
+        if ($this->sharedInfo == null) {
+            $this->sharedInfo = new ShareUnitInterface();
+            $this->setSharedInfo();
+        }
+    }
+
     public function goUnit(Coords $coords){
         $this->coords->setCoords($coords->getX(), $coords->getY());
+    }
+    public function setSharedInfo(){
+        if($this->sharedInfo->sound === null){
+            $this->sharedInfo->sound = 'sound_'.$this->name;
+        }
+
+        if($this->sharedInfo->texture === null){
+            $this->sharedInfo->texture = 'texture_'.$this->name;
+        }
     }
 }
 
 class Tank extends UnitInterface{
     public function __construct(){
+
         $this->weight = 200;
         $this->weapon = true;
         $this->life = 100;
         $this->name = 'tank';
         $this->coords = new Coords(0,0);
+        parent::__construct();
+
     }
+
+
 
 }
 
 class Infantry extends UnitInterface{
     public function __construct(){
+
         $this->weight = 20;
         $this->weapon = false;
         $this->life = 10;
         $this->name = 'infantry';
         $this->coords = new Coords(0,0);
+        parent::__construct();
+
+
     }
 }
 
 class Cavalry extends UnitInterface{
     public function __construct(){
+
         $this->weight = 30;
         $this->weapon = true;
         $this->life = 40;
         $this->name = 'cavalry';
         $this->coords = new Coords(0,0);
+        parent::__construct();
+
+
     }
 }
 
@@ -69,14 +106,16 @@ class Coords{
 abstract class Army{
     public $coords;
     public $units = [];
+
     public function go(Coords $coords){
 
         foreach($this->units as $unit){
             echo $unit->name . " goes to ({$coords->getX()}, {$coords->getY()})\n";
-			$unit->goUnit($coords);
-		}
+            $unit->goUnit($coords);
+        }
     }
     abstract public function addUnit($unit);
+
 }
 
 class ArmyTankInfantry extends Army{
@@ -104,7 +143,7 @@ $army1->addUnit('tank');
 $army1->addUnit('infantry');
 $army1->addUnit('tank');
 $army1->go(new Coords(2,3));
-
+var_dump($army1);
 
 $army1 = new ArmyInfantryCavalry();
 $army1->addUnit('cavalry');
